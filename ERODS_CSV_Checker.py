b72f27c39e7077b770b4ff9f716cca22d0d6799d
Cody Nuenthel <89996557+CNuenthel@users.noreply.github.com>
2023-10-30 12:01:15 -0600

    Update ERODS_CSV_Checker.py
    added simplified directory location

--- ERODS_CSV_Checker.py
+++ ERODS_CSV_Checker.py
@@ -3,10 +3,12 @@ import re
 import os
 import code
 
+# TODO open a print function to allow captured data elements to be printed on text file
+# TODO allow run operation to work in a cyle
 
 WORKING_DIRECTORY = os.path.realpath(__file__)[:-20]
 
-# This setup is function for Motive outputs
+# This setup is functional for outputs that contain solely event list data
 
 class RowData:
     def __init__(self, csv_dict: dict):
@@ -24,9 +26,9 @@ class RowData:
 
 def run():
     dir_list = os.listdir(WORKING_DIRECTORY)
-    files = "\n".join([filename[:-4] for filename in dir_list if filename != "ERODS_CSV_Checker.py"])
-    csv_name = input(f"What is the csv filename that you would like to run...\n{files}\n")
-    pattern = "Inactive|Unidentified|PC"
+    files = "\n".join([filename[:-4] for filename in dir_list if filename[-4:] == ".csv"])
+    csv_name = input(f"What is the csv filename that you would like to run...\n{files}\n\n")
+    pattern = "Inactive|Unidentified|PC|Active"
     csv_data = []
 
     try:
@@ -34,18 +36,24 @@ def run():
         with open(csv_path, newline='') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
+
+        # Catch odd ELD csv output
+                if 'ELD File Header Segment:' in row:
+                    print("Header file detected, this data format cannot be read by this script")
+                    break
+
                 item = RowData(row)
                 if re.search(pattern, item.status):
                     print(item.date, item.time_stamp, item.status)
                 csv_data.append(item)
-    
+
         print("Complete")
 
     except FileNotFoundError:
         dir_list = os.listdir(WORKING_DIRECTORY)
         files = "\n".join([filename for filename in dir_list if filename != "ERODS_CSV_Checker.py"])
-        
-        print(f"That csv filename was not located in the current directory, here are the files in the directory:\n {files}")
+
+        print(f"That csv filename was not located in the current directory, please try again, selecting a valid .csv file in this directory")
 
 def delete():
     csv_name = input("What is the csv filename that you would like to delete?\n")
@@ -53,5 +61,6 @@ def delete():
     print(f"CSV file has been deleted!")
 
 if __name__ == "__main__":
-    print("Welcome to the ERODS CSV Checker! Type run() to complete an ERODS csv check.\n\n")
+    print("Welcome to the ERODS CSV Checker!\n\n")
+    run()
     code.interact(local=locals())
