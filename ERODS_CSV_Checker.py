b7c8d80b4df12069ca0eecbb9beea12297d36fe7
CLN <cnuenthel@gmail.com>
2023-12-11 14:45:20 -0600

    CLI Updates
    Added CLI color indicators for visibility, system quit option

--- ERODS_CSV_Checker.py
+++ ERODS_CSV_Checker.py
@@ -1,15 +1,26 @@
 import csv
 import re
 import os
+import logging
+import sys
+from pathlib import Path
+from colorama import init, Fore, Style
+import win32print
+import win32ui
 
 # TODO open a print function to allow captured data elements to be printed on text file
-# TODO allow run operation to work in a cyle
+# TODO allow run operation to work in a cycle
 
-WORKING_DIRECTORY = os.path.join(os.path.dirname(os.path.abspath(__file__)), "erods")
+# Establish standard path for erods file reading
+WORKING_DIRECTORY = Path(os.getcwd())
+
+
+# Helper function to sort and return a string into only its numbers
+def format_number_string(num_str: str) -> int:
+    return int(re.sub(r',', '', num_str))
 
 
 # This setup is functional for outputs that contain solely event list data
-
 class RowData:
     def __init__(self, csv_dict: dict):
         self.date = csv_dict['Date']
@@ -24,91 +35,124 @@ class RowData:
     def __str__(self):
         return f"Date: {self.date}, Time: {self.time_stamp}, Coord: {self.coord}, Odo: {self.odometer}, Eng Hours: {self.engine_hours}, Status: {self.status}, Input: {self.input_unit}, Id: {self.id}"
 
-def run():
-    dir_list = os.listdir(WORKING_DIRECTORY)
 
-    operation = input("\nPlease select an operation:\n1.Run\n2.Delete\n")
+def run():
+    csv_list = os.listdir(WORKING_DIRECTORY / "erods")
+
+    operation = input(f"\n{Fore.BLUE + 'Please select an operation'}:\n"
+                      f"--------------------------{Style.RESET_ALL}\n"
+                      f"1.{Fore.GREEN + 'Run'}{Style.RESET_ALL}\n"
+                      f"2.Delete CSV\n"
+                      f"3.Quit\n"
+                      f"{Fore.BLUE + '--------------------------'}{Style.RESET_ALL}\n")
 
     if operation.lower() in ["run", "1"]:
-        num_file_dict = {str(i): item for i, item in enumerate(dir_list, 1)}
+        data = []
+        num_file_dict = {str(i): item for i, item in enumerate(csv_list, 1)}
         file_str = "\n".join([f"{i}. {filename}" for i, filename in num_file_dict.items()])
-        
-        csv_number = input(f"What is the number for the csv that you would like to run...\n{file_str}\n\n")
-        csv_path = f"{WORKING_DIRECTORY}\\{num_file_dict[csv_number]}"
-    
+
+        csv_number = input(f"\n{Fore.BLUE + 'What is the name of the erods csv that you would like to run?'}\n"
+                           f"---------------------------------------------------------------{Style.RESET_ALL}"
+                           f"\n{file_str}\n"
+                           f"{Fore.BLUE + '---------------------------------------------------------------'}{Style.RESET_ALL}\n")
+
+        csv_path = f"{WORKING_DIRECTORY / 'erods'}\\{num_file_dict[csv_number]}"
+
         pattern = "Inactive|Unidentified|PC|Active"
-        
-        non_driving_categories = ["Sleeper Berth", "Power-up", "On-duty, not driving", "Off-duty", "PC/YM Cleared", "Shut-down"]
+
+        non_driving_categories = ["Sleeper Berth", "Power-up", "On-duty, not driving", "Off-duty", "PC/YM Cleared",
+                                  "Shut-down"]
         driving_categories = ["Driving", "Intermediate log", "PC"]
-        
-        try:
-            with open(csv_path, newline='') as csvfile:
-                reader = csv.DictReader(csvfile)
 
-                last_status = None
-                last_odo = 0
-                moving = None
-                
-                for row in reader:
-            # Catch odd ELD csv output
-                    if 'ELD File Header Segment:' in row:
-                        print("Header file detected, this data format cannot be read by this script")
-                        break
+        with open(csv_path, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
 
-                    # Check for pattern keywords in RODs
-                    item = RowData(row)
-                    if re.search(pattern, item.status):
-                        print(item.date, item.time_stamp, item.status)
+            last_odo = 0
+            moving = False
+            for row in reader:
+                # Catch odd ELD csv output
+                if 'ELD File Header Segment:' in row:
+                    print("Header file detected, this data format cannot be read by this script")
+                    break
 
-                    if last_odo == 0 and item.odometer:
+                # Check for pattern keywords in RODs
+                item = RowData(row)
+                if re.search(pattern, item.status):
+                    data.append(f"{item.date} {item.time_stamp} {item.status}")
+                    print(item.date, item.time_stamp, item.status)
+
+                if last_odo == 0 and item.odometer:
+                    last_odo = item.odometer
+
+                # Continuously update odometer in moving categories
+                if item.status in driving_categories and item.odometer:
+                    if not moving:
+                        if item.odometer != last_odo:
+                            moving = True
+                            if item.odometer != "Missing power-up event" and last_odo != "Missing power-up event":
+                                delta = format_number_string(item.odometer) - format_number_string(last_odo)
+
+                                if delta not in [-1, 1]:
+                                    data.append(
+                                        f"{item.date} {item.time_stamp} {item.status} {last_odo} -> {item.odometer} [{delta} miles] Possible Odo Skip")
+                                    print(item.date, item.time_stamp, item.status, last_odo, "->", item.odometer,
+                                          f"[{delta} miles]", "Possible Odo Skip")
+                                    continue
+                                elif delta in [-1, 1]:
+                                    continue
+
+                            data.append(f"{item.date} {item.time_stamp} {item.status} {last_odo} {item.odometer}")
+                            print(item.date, item.time_stamp, item.status, last_odo, item.odometer,
+                                  "Possible Odo Skip")
+
+                    if item.odometer != "Missing power-up event":
                         last_odo = item.odometer
 
-                    # Continuously update odometer in moving categories
-                    if item.status in driving_categories and item.odometer:
-                        if moving == False:
-                        
-                            if item.odometer != last_odo:
-                                moving = True
-                                print(item.date, item.time_stamp, item.status, last_odo, item.odometer, "Possible Odo Skip")
-                            
-                        if item.odometer != "Missing power-up event":
-                            last_odo = item.odometer
-                        
-                        moving = True
-                        continue 
+                    moving = True
+                    continue
 
-                    elif item.status in non_driving_categories and item.odometer:
-                            
-                        if moving == False:
-                            continue
-                        # First instantiation of end of movement
-                        if item.odometer != "Missing power-up event":
-                            last_odo = item.odometer
-                        moving = False
-                        continue 
-                        
-            print("Complete\n\n")
+                elif item.status in non_driving_categories and item.odometer:
 
-        except FileNotFoundError:
-            dir_list = os.listdir(WORKING_DIRECTORY)    
-            print("That csv filename was not located in the current directory, please try again, selecting a valid .csv file in this directory")
+                    if not moving:
+                        continue
+                    # First instantiation of end of movement
+                    if item.odometer != "Missing power-up event":
+                        last_odo = item.odometer
+
+                    moving = False
+                    continue
+
+        print(f"\n** {Fore.MAGENTA + 'Complete'} ** {Style.RESET_ALL}\n\n")
 
     elif operation.lower() in ["delete", "2"]:
-        num_file_dict = {str(i): item for i, item in enumerate(dir_list, 1)}
+        csv_list.append("Cancel")
+        num_file_dict = {str(i): item for i, item in enumerate(csv_list, 1)}
         file_str = "\n".join([f"{i}. {filename}" for i, filename in num_file_dict.items()])
-        csv_number = input(f"What is the number fo the csv that you would like to delete...\n{file_str}\n\n")
-        csv_path = f"{WORKING_DIRECTORY}\\{num_file_dict[csv_number]}"
+
+        csv_number = input(f"\n{Fore.BLUE + 'Select a csv file to delete or cancel.'}\n"
+                           f"--------------------------------------------------------------{Style.RESET_ALL}\n"
+                           f"{file_str}\n"
+                           f"{Fore.BLUE + '--------------------------------------------------------------'}{Style.RESET_ALL}\n")
+
+        if num_file_dict[csv_number] == "Cancel":
+            return
+
+        csv_path = f"{WORKING_DIRECTORY / 'erods'}\\{num_file_dict[csv_number]}"
+
         try:
             os.remove(csv_path)
             print(f"{num_file_dict[csv_number]} has been deleted")
         except FileNotFoundError:
             print(f"Sorry the file {num_file_dict[csv_number]} was not found.")
 
+    elif operation.lower() in ["quit", "3"]:
+        sys.exit()
+
     else:
-        print("Please select an operation, Run or Delete")
+        print("Please select a valid operation, Run, Delete, or Quit")
 
 
 if __name__ == "__main__":
-    print("Welcome to the ERODS CSV Checker!\n\n")
+    print("Welcome to the ERODS CSV Checker!")
     while True:
         run()
